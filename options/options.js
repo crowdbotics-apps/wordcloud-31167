bc67dbb208d5ee63bb407e714bad77c2bf5fd525
Crowdbotics <team@crowdbotics.com>
2021-10-06 16:44:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wordcloud_31167",
+  url: "https://wordcloud_31167.botics.co",
+  api: "https://wordcloud_31167.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
